2995e632977bfe4fb97a9d193a0f90ae33e349a7
bytrustu <bytrustu@gmail.com>
2022-12-27 19:58:48 +0900

    fix: test 실패 case

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,8 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const heading = screen.getByRole('heading');
+  expect(heading).toBeInTheDocument();
+  expect(heading.textContent).toBe('Learn CI');
 });
